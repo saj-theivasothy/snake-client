53050303184cf8f462d2c5b4be51d3e1dd2f3dfb
Sayinthan Theivasothy <t.saj1994@gmail.com>
2020-03-11 16:44:46 +0000

    client sends message to server upon connection

--- client.js
+++ client.js
@@ -14,6 +14,10 @@ const connect = function() {
     console.log(data);
   })
   
+  conn.on('connect', () => {
+    console.log("Successfully connected to game server")
+    conn.write("Name: ST9");
+  })
   // interpret incoming data as text
   conn.setEncoding('utf8'); 
 
